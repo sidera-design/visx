fb9bbcadc00325bc59c358e245c8cabb96b50fec
Hoshino Hitoshi <hoshino.hitoshi@sidera.jp>
2024-02-19 06:01:00 +0000

    remove artifacts for TS

--- packages/timing-diagram/lib/TimingDiagram.d.ts
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-import { XYChartProps } from '@visx/xychart/lib/components/XYChart';
-import { LinearScaleConfig } from '@visx/scale';
-export type TimingDiagramProps<Datum extends object> = XYChartProps<LinearScaleConfig<number>, LinearScaleConfig<number>, Datum>;
-export default function TimingDiagram(): React.JSX.Element;
--- packages/timing-diagram/lib/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TimingDiagram = void 0;
-var TimingDiagram_1 = require("./TimingDiagram");
-Object.defineProperty(exports, "TimingDiagram", { enumerable: true, get: function () { return __importDefault(TimingDiagram_1).default; } });
